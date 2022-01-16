120bc8a94dac58920a5b03f95ab96aecc7c4480c
cesarcg01 <cesar.cochachin@softtekprojects.com>
2022-01-16 00:50:36 -0500

    lint

--- my-store/src/app/components/img/img.component.ts
+++ my-store/src/app/components/img/img.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit ,Input ,Output ,EventEmitter, OnChanges, AfterViewIni
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit ,OnChanges, AfterViewInit, OnDestroy{
+export class ImgComponent  {
   img: string= '';
+  // eslint-disable-next-line @angular-eslint/no-input-rename
   @Input('img')
   set changeImg(newImg: string){
     this.img = newImg;
@@ -25,34 +26,6 @@ export class ImgComponent implements OnInit ,OnChanges, AfterViewInit, OnDestroy
     //No async --once time
     // console.log('constructor','imgValue =>', this.img);
   }
-  ngOnChanges(){
-    //before - during render
-    //changes inputs -- multiples times
-    // console.log('ngOnChanges','imgValue =>', this.img);
-  }
-  ngOnInit(): void {
-    //before render
-    //async -- fetch -- once time
-    // console.log('ngOnInit','imgValue =>', this.img);
-    // this.counterFn = window.setInterval(() =>{
-    //   this.counter +=1;
-    //   console.log('run counter');
-    // },1000);
-
-  }
-
-  ngAfterViewInit(){
-    //after render
-    //handler children --once time
-    // console.log('AfterViewInit');
-  }
-
-
-  ngOnDestroy(){
-    // console.log('ngOnDestroy');
-    // window.clearInterval(this.counterFn);
-  }
-
   imgError(){
   this.img = this.imageDefault;
   }
