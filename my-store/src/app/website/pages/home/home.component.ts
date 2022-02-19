e7099e87a116e7ec011caf3c2e5e7b8b0ad0ffff
cesarcg01 <cesar.cochachin@softtekprojects.com>
2022-02-19 12:02:53 -0500

    Vistas anidadas

--- my-store/src/app/website/pages/home/home.component.ts
+++ my-store/src/app/website/pages/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../../services/products.service';
+import { ProductsService } from '../../../services/products.service';
 import { ActivatedRoute } from '@angular/router';
 
-import { Product } from '../../models/product.model';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-home',
