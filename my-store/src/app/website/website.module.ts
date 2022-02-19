6adce33c33d3ba6ab7c99fe80f75b1364d69d8c2
cesarcg01 <cesar.cochachin@softtekprojects.com>
2022-02-19 17:03:48 -0500

    Precarga de módulos

--- my-store/src/app/website/website.module.ts
+++ my-store/src/app/website/website.module.ts
@@ -4,9 +4,7 @@ import { CommonModule } from '@angular/common';
 import { WebsiteRoutingModule } from './website-routing.module';
 import { NavComponent } from './components/nav/nav.component';
 import { SharedModule } from './../shared/shared.module';
-
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -21,7 +19,6 @@ import { SwiperModule } from 'swiper/angular';
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     MyCartComponent,
     LoginComponent,
     RegisterComponent,
